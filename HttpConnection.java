04b36b5f08044c5bf90a7c0afb069c727fda8f14
Chul Ho Shin <90623647+886814@users.noreply.github.com>
2022-12-25 00:02:22 +0900

    Add files via upload

--- HttpConnection.java
+++ HttpConnection.java
@@ -1,58 +1,75 @@
 package honghong;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.net.HttpURLConnection;
+import java.io.File;
+import java.io.FileInputStream;
 import java.net.URL;
+import java.net.URI;
 
-import org.json.JSONObject;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
 
 public class HttpConnection {
+
+    public static String filePath = "C:/workspace/";
+    public static String fileNm = "스타뱅킹_질문리스트.xlsx";
+
     public static void main(String[] args) {
-        post();
-    }
+        qaPost();
 
-    public static void post() {
-        try {
-            URL url = new URL("http://localhost:8080/rest/post");
-            HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+        public static void qaPost () thors Exception {
 
-            conn.setRequestMethod("POST"); // http 메서드
-            conn.setRequestProperty("Content-Type", "application/json"); // header Content-Type 정보
-            conn.setDoInput(true); // 서버에 전달할 값이 있다면 true
-            conn.setDoOutput(true); // 서버로부터 받는 값이 있다면 true
+            try {
+                // 엑셀에서 데이터 읽어오기
+                FileInputStream file = new FileInputSTream(new File(filePath, fileName));
+                XSSFWorkbook workbook = new XSSFWorkbook(file);
+                XSSFSheet sheet = workbook.getSheetAt(0);
+                Iterator<Row> rowIterator = sheet.iterator();
+                int rowIndex = 1;
 
-            // 서버에 데이터 전달
-            JSONObject obj = new JSONObject();
-            obj.put("searchWord", "메가셀러론");
+                // row 한줄씩 읽기
+                while (rowIterator.hasNext()){
+                    Row row = rowIterator.next();
+                    Cell cell = row.getCell(4); // 5번째 컬럼 읽기
 
-            try (OutputStream os = conn.getOutputStream()){
-                byte request_data[] = obj.toString().getBytes("utf-8");
-                os.write(request_data);
-                os.close();
-            }
-            catch(Exception e) {
-                e.printStackTrace();
-            }
+                    //서버에 데이터 보내기
+                    String url = "http://10.38.136.119.8060/";
+                    URI uri = UriComponentsBuilder.fromHttpUrl(url).path("api/func/product/kbProductQa").queryParam("pageSize", "5").queryParam("searchWord", cell.getStringCellValue()).queryParam("svcDstcd", "02")
+                            .encode().build().toUri();
 
+                    //서버에서 데이터 받기
+                    RestTemplate restTemplate = new RestTemplate();
+                    RequestEntity<Void> req = RequestEntity.get(uri).build();
+                    ResponseEntity<String> result = restTemplate.exchange(req,String.class);
 
-            // 서버로부터 데이터 읽어오기
-            BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-            StringBuilder sb = new StringBuilder();
-            String line = null;
+                    JSONParser parser = new JSONParser();
+                    JSONObject jsonObj = (JSONObject) parser.parse(result.getBody());
+                    JSONObject json = (JSONObject) jsonObj.get("data");
 
-            while((line = br.readLine()) != null) { // 읽을 수 있을 때 까지 반복
-                sb.append(line);
-            }
-
-            System.out.println(sb.toString());
-
-        } catch (Exception e) {
-            e.printStackTrace();
+                    try{
+                        JSONArray jsonArray = (JSONArray) json.get("resultList");
+                        String res = "";
+                        for (int i=0; i<jsonArray.size();i++){
+                            JSONObject objectInArray = (JSONObject) jsonArray.get(i);
+                            res += ", " + objectInArray.get("brandPrdtCd") + "|" + objectInArray.get("prdctName");
+                        }
+                        res = res.substring(2);
+                        sheet.getRow(rowIndex).createCell(5).setCellValue(res);
+                    } catch (Exception e){
+                        sheet.getRow(rowIndex).createCell(5).setCellValue("답변 없음");
+                    }
+                } file.close();
+            } catch (Exception e){
+                throw e;
         }
     }
 }
